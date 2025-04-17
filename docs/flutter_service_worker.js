4041540401b4bc9bbfed8de7eefb517e501d1b10
Abdulrhman Alattas <kzama777@gmail.com>
2025-04-17 22:03:37 +0300

    تحديث ملفات الموقع

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a35fd9b5336dfbbb68496018d86075be",
+const RESOURCES = {"flutter_bootstrap.js": "3bf2f6af0078c5d50e9fac333d64dc7a",
 "version.json": "2f3cc9d7f131098aca4eb2e52e21c891",
 "index.html": "82f6ecfa4754bea81bef983ced2721fc",
 "/": "82f6ecfa4754bea81bef983ced2721fc",
